d6ce5aeeceabfee94384905540cb5d288c494442
Slaieet <sl4ieet@gmail.com>
2024-04-11 13:45:17 -0600

    responsive design to MovieInfo, on the y axis

--- src/components/subcomponents/MovieInfo.jsx
+++ src/components/subcomponents/MovieInfo.jsx
@@ -3,37 +3,48 @@ import { IMG_PATH } from '../../logic/constants.js'
 
 export default function MovieInfo ({ info }) {
   return createPortal(
-    <section className='center-absolute w-[80%] bg-darkbgTwo flex'>
-      <header className='h-full w-[325px] bg-purple-300 shrink-0'>
-        <img
-          src={IMG_PATH + info.imgEndpoint}
-          alt='movie'
-          className='w-full min-h-[500px] object-cover'
-        />
-      </header>
-      <article className='min-h-[500px] grow-[1] p-5 relative'>
+    <div
+      className='h-full min-h-[600px] w-full absolute top-0 left-0 bg-red-500 flex justify-center
+      items-center z-[150]'
+    >
+      <section className='w-[80%] h-[500px] bg-darkbgTwo flex z-[100]'>
+        <header className='w-[325px] h-full shrink-0 self-center'>
+          <img
+            src={IMG_PATH + info.imgEndpoint}
+            alt='movie'
+            className='w-full h-full object-cover'
+          />
+        </header>
+        <article className='grow-[1] p-5 relative h-full flex flex-col justify-between gap-8 overflow-y-auto'>
 
-        <div>
-          <h6 className='text-[1.5rem] text-center font-bold'>{info.title}</h6>
-          <ul className='flex justify-evenly mt-6 opacity-90'>
-            {(info.genresNames).map(genre => {
-              return (
-                <li key={genre.id}>{genre.name}</li>
-              )
-            })}
-          </ul>
+          <div className='shrink-0'>
+            <h6 className='text-[1.5rem] text-center font-bold'>{info.title}</h6>
+            <ul className='flex justify-evenly opacity-90 mt-6'>
+              {(info.genresNames).map(genre => {
+                return (
+                  <li key={genre.id}>{genre.name}</li>
+                )
+              })}
+            </ul>
 
-        </div>
+          </div>
 
-        <p className='mt-10'>{info.overview}</p>
+          <div className='grow-[1]'>
+            <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, eveniet magni sit itaque, enim non similique ea fugiat officia quia beatae praesentium? Vitae ipsam quia quod laboriosam commodi numquam ad.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta numquam pariatur, quam facere odio eaque architecto eius amet dignissimos explicabo illum possimus ea quas, quaerat commodi obcaecati nulla ad debitis!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, iusto aliquam. Distinctio asperiores, assumenda culpa ea saepe beatae quibusdam harum, eos vitae magnam aliquid non! Veniam ipsam error quae perferendis?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat debitis et distinctio! Explicabo expedita aliquid ea voluptas error? Eaque placeat deleniti consequatur quis odio unde hic? Facere iusto vitae obcaecati
+            </p>
+          </div>
 
-        <footer className='absolute w-[90%] bottom-5 flex justify-evenly text-[0.9rem]'>
-          <p>Fecha de lanzamiento: {info.release__date}</p>
-          <p>Promedio de votos: {info.vote_average}</p>
-        </footer>
+          <footer className='flex justify-evenly text-[0.9rem] shrink-0'>
+            <p>Fecha de lanzamiento: {info.release__date}</p>
+            <p>Promedio de votos: {info.vote_average}</p>
+          </footer>
 
-      </article>
-    </section>,
+        </article>
+      </section>
+    </div>,
     document.getElementById('movie-info')
   )
 }
